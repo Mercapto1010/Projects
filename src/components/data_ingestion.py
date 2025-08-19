c0be9069970709e99cb0501b46d5aa25b57dd77a
prateekpathak <pathakyash0610@gmail.com>
2025-08-20 02:37:43 +0530

    commited

--- src/components/data_ingestion.py
+++ src/components/data_ingestion.py
@@ -8,4 +8,39 @@ from dataclasses import dataclass
 
 @dataclass
 class DataIngestionConfig:
-    pass
+    train_data_path= str=os.path.join('artifacts','train.csv')
+    test_data_path= str=os.path.join('artifacts','test.csv')
+    raw_data_path= str=os.path.join('artifacts','data.csv')
+
+ 
+class DataIngestion:
+    def __init__(self):
+        self.ingestion_config= DataIngestionConfig()  #takes all the three paths above
+
+    def initiate_data_ingestion(self):  #extract the data from the database(e.g.MongoDB SQL etc)
+         logging.info("Entered the data ingestion method or component")
+         try:
+             df=pd.read_csv('notebook/data/stud.csv')
+             logging.info("Read the dataset as Dataframe")
+             os.makedirs(os.path.dirname(self.ingestion_config.train_data_path),exist_ok=True)
+             
+             df.to_csv(self.ingestion_config.raw_data_path,index=False,header=True)
+             logging.info("Train test split initiated")
+
+             train_set,test_set=train_test_split(df,test_size=0.20,random_state=42)
+             train_set.to_csv(self.ingestion_config.train_data_path,index=False,header=True)
+             test_set.to_csv(self.ingestion_config.test_data_path,index=False,header=True)
+             logging.info("Ingestion of data is complete")
+
+             return(
+                 self.ingestion_config.train_data_path,
+                 self.ingestion_config.test_data_path
+
+             )
+         except Exception as e:
+             raise CustomException(e,sys)
+         
+
+if __name__ == "__main__":
+    obj=DataIngestion()
+    obj.initiate_data_ingestion()
